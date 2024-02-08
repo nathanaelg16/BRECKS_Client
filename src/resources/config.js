11cc5dda12101fc7d374ec9ea8684b24b3be22fb
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-02-08 08:39:00 -0500

    modifying warning color palette

--- src/resources/config.js
+++ src/resources/config.js
@@ -58,6 +58,16 @@ export const theme = extendTheme({
                     "solidDisabledBg": "var(--joy-palette-neutral-200)"
                 },
                 "warning": {
+                    "50": "#fefce8",
+                    "100": "#fef9c3",
+                    "200": "#fef08a",
+                    "300": "#fde047",
+                    "400": "#facc15",
+                    "500": "#eab308",
+                    "600": "#ca8a04",
+                    "700": "#a16207",
+                    "800": "#854d0e",
+                    "900": "#713f12",
                     "solidDisabledBg": "var(--joy-palette-neutral-200)"
                 }
             }
