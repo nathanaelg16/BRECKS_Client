da64f41351ea2d70cc0836ca6985feeb9b68b5d5
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-02-08 08:41:51 -0500

    initial work with change status endpoint

--- src/app/(app)/(home)/job_card.js
+++ src/app/(app)/(home)/job_card.js
@@ -1,9 +1,9 @@
-import {Badge, Card, CardActions, CardContent, Divider, IconButton, Tooltip} from "@mui/joy";
+import {Badge, Card, CardActions, CardContent, Divider, Modal, Tooltip} from "@mui/joy";
 import Typography from "@mui/joy/Typography";
 import {useEffect, useState} from "react";
 import {postman} from "@/resources/config";
 import {Rubik} from "next/font/google";
-import {ErrorOutline, PriorityHigh, Warning} from "@mui/icons-material";
+import {ErrorOutline} from "@mui/icons-material";
 import Button from "@mui/joy/Button";
 import {useRouter} from "next/navigation";
 
@@ -11,6 +11,7 @@ const rubik = Rubik({subsets: ['latin']})
 
 export default function JobCard({job}) {
     const router = useRouter()
+    const [open, setOpen] = useState(false)
 
     const [stats, setStats] = useState({
         totalManDays: -1,
@@ -48,24 +49,29 @@ export default function JobCard({job}) {
         })
     }, [stats])
 
-    return <Badge {...badgeProps} size='lg'>
-        <Card color='primary'>
-            <Typography level='h4' fontSize='md' textAlign='center'>{job.address}</Typography>
-            <Typography sx={{mt: -1}} level='body-sm' textAlign='center'>{job.identifier && `(${job.identifier})`}</Typography>
-            <CardContent>
-                <Divider className={rubik.className} sx={{my: 1, mb: 0}}>THIS WEEK</Divider>
-                <Typography level='body-sm' sx={{fontWeight: '600'}}>Total Man-days: <Typography sx={{fontWeight: '200'}}>{stats.totalManDays}</Typography></Typography>
-                <Typography level='body-sm' sx={{fontWeight: '600'}}>Avg Manpower: <Typography sx={{fontWeight: '200'}}>{stats.avgDailyManPower}</Typography></Typography>
-                <Typography color={'warning'} level='body-sm' sx={{fontWeight: '600'}}>Missing Reports: <Typography sx={{fontWeight: '200'}}>{stats.missingReportDates.length}</Typography></Typography>
-            </CardContent>
-            <CardActions>
-                <Button sx={{background: '#00A550', "&:hover": {background: '#00612F'}}} onClick={() => router.push(`/report?job=${job.id}`)}>
-                    Add Report
-                </Button>
-                <Button sx={{background: '#2072AF', "&:hover":{background: '#11466F'}}}>
-                    Change Status
-                </Button>
-            </CardActions>
-        </Card>
-    </Badge>
+    return <>
+        <Badge {...badgeProps} size='lg'>
+            <Card color='primary'>
+                <Typography level='h4' fontSize='md' textAlign='center'>{job.address}</Typography>
+                <Typography sx={{mt: -1}} level='body-sm' textAlign='center'>{job.identifier && `(${job.identifier})`}</Typography>
+                <CardContent>
+                    <Divider className={rubik.className} sx={{my: 1, mb: 0}}>THIS WEEK</Divider>
+                    <Typography level='body-sm' sx={{fontWeight: '600'}}>Total Man-days: <Typography sx={{fontWeight: '200'}}>{stats.totalManDays}</Typography></Typography>
+                    <Typography level='body-sm' sx={{fontWeight: '600'}}>Avg Manpower: <Typography sx={{fontWeight: '200'}}>{stats.avgDailyManPower}</Typography></Typography>
+                    <Typography color={stats.missingReportDates.length > 0 ? 'primary' : ''} level='body-sm' sx={{fontWeight: '600'}}>Missing Reports: <Typography sx={{fontWeight: '200'}}>{stats.missingReportDates.length}</Typography></Typography>
+                </CardContent>
+                <CardActions>
+                    <Button sx={{background: '#00A550', "&:hover": {background: '#00612F'}}} onClick={() => router.push(`/report?job=${job.id}`)}>
+                        Add Report
+                    </Button>
+                    <Button sx={{background: '#2072AF', "&:hover":{background: '#11466F'}}} onClick={() => setOpen(true)}>
+                        Change Status
+                    </Button>
+                </CardActions>
+            </Card>
+        </Badge>
+        <Modal open={open}>
+
+        </Modal>
+    </>
 }
