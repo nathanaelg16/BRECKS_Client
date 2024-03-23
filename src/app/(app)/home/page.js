8df16ae389fa9fd8f9d1ee360d49502a4c5d50b8
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-03-23 13:16:35 -0400

    removing user-centric views

--- src/app/(app)/home/page.js
+++ src/app/(app)/home/page.js
@@ -1,16 +1,14 @@
 'use client'
 
 import Typography from "@mui/joy/Typography";
-import {Sheet, Stack} from "@mui/joy";
-import {useContext, useEffect, useState} from "react";
+import {Stack} from "@mui/joy";
+import {useEffect, useState} from "react";
 import {postman} from "@/resources/config";
-import {UserContext} from "@/app/(app)/context";
 import JobAccordion from "@/app/(app)/home/job_accordion";
 import SentimentVeryDissatisfiedSharpIcon from '@mui/icons-material/SentimentVeryDissatisfiedSharp';
+import Box from "@mui/joy/Box";
 
 export default function Home() {
-  const user = useContext(UserContext)
-  const [userJobs, setUserJobs] = useState({})
   const [allJobs, setAllJobs] = useState({})
   const [error, setError] = useState(false)
 
@@ -24,37 +22,23 @@ export default function Home() {
   }
 
   useEffect(() => {
-    postman.get('/jobs?' + new URLSearchParams({teamID: user.teamID})).then((response) => {
-      if (response.status === 200) setUserJobs(categorize(response.data.sort((a, b) => a.address.localeCompare(b.address))))
-      else throw new Error()
-    }).catch(() => setError(true))
-
     postman.get('/jobs').then((response) => {
       if (response.status === 200) setAllJobs(categorize(response.data))
       else throw new Error()
     }).catch(() => setError(true))
-  }, [user])
+  }, [])
 
   const onError = <Stack justifyContent='center' alignItems='center' spacing={2} sx={{height: 1}}>
     <SentimentVeryDissatisfiedSharpIcon color='primary' sx={{fontSize: 48}} />
     <Typography level='h3' color='primary' textAlign='center'>An error occurred accessing the application. Please try again later.</Typography>
   </Stack>
 
-  const home = <Stack spacing={4} sx={{mb: 3}} useFlexGap>
-    <Sheet id={'user_jobs'} sx={{p: 2}}>
-      <Typography level={'h2'} sx={{mb: 1}}>Your Jobs</Typography>
-      <JobAccordion jobs={userJobs} />
-    </Sheet>
-    <Sheet id={'all_jobs'} sx={{p: 2}}>
-      <Typography level={'h2'} sx={{mb: 1}}>All Jobs</Typography>
-      <JobAccordion jobs={allJobs} />
-    </Sheet>
-  </Stack>
+  const home = <Box id={'all_jobs'} sx={{px: 2}}>
+    <JobAccordion jobs={allJobs} />
+  </Box>
 
   return <Stack sx={{width: 1, height: 1}}>
-    <Stack direction='row' justifyContent='space-between'>
-      <Typography level={'h1'} sx={{m: 2, color: 'var(--joy-palette-primary-800)'}}>Dashboard</Typography>
-    </Stack>
+    <Typography level={'h1'} sx={{m: 2, color: 'var(--joy-palette-primary-800)'}}>Dashboard</Typography>
     {error ? onError : home}
   </Stack>
 }
--- src/app/(app)/job_picker.js
+++ src/app/(app)/job_picker.js
@@ -1,36 +1,24 @@
 import Typography from "@mui/joy/Typography";
-import {Divider, Option, Select} from "@mui/joy";
+import {Option, Select} from "@mui/joy";
 import Box from "@mui/joy/Box";
-import {useContext, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {postman} from "@/resources/config";
-import {UserContext} from "@/app/(app)/context";
 import {useRouter} from "next/navigation";
 
 export default function JobPicker(props) {
     const router = useRouter()
-    const [userJobs, setUserJobs] = useState([])
     const [allJobs, setAllJobs] = useState([])
-    const user = useContext(UserContext);
 
     useEffect(() => {
         postman.get('/jobs?' + new URLSearchParams({status: 'ACTIVE'}))
             .then((response) => {
-                if (response.status === 200) setAllJobs(response.data)
+                if (response.status === 200) setAllJobs([...response.data].sort())
                 else console.log('hmmm')
             }).catch((error) => {
                 console.log(error)
                 //todo implement error handling
             })
-
-        postman.get('/jobs?' + new URLSearchParams({status: 'ACTIVE', teamID: user.teamID}))
-            .then((response) => {
-                if (response.status === 200) setUserJobs(response.data)
-                else console.log('hmmm')
-            }).catch((error) => {
-                console.log(error)
-                //todo implement error handling
-            })
-    }, [user])
+    }, [])
 
     const generateOptions = (jobs) => {
         return jobs.map((job) => <Option key={job.id} value={job.id}>{job.address}</Option>)
@@ -44,9 +32,6 @@ export default function JobPicker(props) {
             <Select value={''} sx={{marginRight: 1}} placeholder='Select a jobsite...' onChange={(ev, nv) => {
                 if (nv !== null && nv !== '') router.push(`/job/${nv}`)
             }}>
-                <Divider>Your Jobs</Divider>
-                {userJobs.length > 0 ? generateOptions(userJobs) : <Option disabled key={-1} value={-1}>No jobs available</Option>}
-                <Divider>All Active</Divider>
                 {allJobs.length > 0 ? generateOptions(allJobs) : <Option disabled key={-2} value={-1}>No jobs available</Option>}
             </Select>
         </Box>
