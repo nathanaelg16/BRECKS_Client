27991f0efbdbc80128bcc22c4558717695444c42
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-03-25 12:15:38 -0400

    error handling

--- src/app/(app)/job/[id]/layout.js
+++ src/app/(app)/job/[id]/layout.js
@@ -1,23 +1,23 @@
 'use client'
 
 import {JobContext} from "@/app/(app)/job/[id]/job_context"
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useContext, useEffect, useState} from "react";
 import {postman} from "@/resources/config";
+import {SnackbarContext} from "@/app/(app)/context";
 
 export default function JobLayout({params, children}) {
+    const setSnackbar = useContext(SnackbarContext)
     const [job, setJob] = useState({})
 
     const updateJob = useCallback(() => {
         postman.get(`/jobs/${params.id}`)
             .then((response) => {
-                if (response.status === 200) setJob(response.data)
-                else {
-                    // todo error handling
-                }
+                setJob(response.data)
             }).catch((error) => {
-            // todo implement error handling
-        })
-    }, [params.id, setJob])
+                if (error.response?.status === 404) setSnackbar('error', {text: '404 - Job site not found', vertical: 'top', horizontal: 'center'})
+                else setSnackbar('error', {text: 'An unexpected error occurred retrieving job information from the database.', vertical: 'top', horizontal: 'center'})
+            })
+    }, [params.id, setJob, setSnackbar])
 
     useEffect(() => {
         updateJob()
