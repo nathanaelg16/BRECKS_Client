3d7946dc79862767da6223ee0e0afd56a555c3de
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-03-25 13:53:38 -0400

    code refactoring, optimizations, and moving to Luxon date API

--- src/app/(app)/job/[id]/(calendar)/calendar.js
+++ src/app/(app)/job/[id]/(calendar)/calendar.js
@@ -1,7 +1,7 @@
 import Box from "@mui/joy/Box";
 import CalendarControl from "@/app/(app)/job/[id]/(calendar)/calendar_control";
 import {Red_Hat_Display} from "next/font/google";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useMemo, useState} from "react";
 import Typography from "@mui/joy/Typography";
 import {Range} from "immutable"
 import CalendarDate from "@/app/(app)/job/[id]/(calendar)/calendar_date";
@@ -9,142 +9,127 @@ import {JobContext} from "@/app/(app)/job/[id]/job_context";
 import {postman} from "@/resources/config";
 import {useRouter} from "next/navigation";
 import ReportViewer from "@/app/(app)/job/[id]/(report)/report_viewer";
+import {SnackbarContext} from "@/app/(app)/context";
 
 const RedHatFont = Red_Hat_Display({subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800']})
+const {DateTime} = require('luxon')
 
 export default function Calendar({sx, calendarState, stats}) {
-    let [job, _] = useContext(JobContext)
+    const setSnackbar = useContext(SnackbarContext)
+    const [job, _] = useContext(JobContext)
+
     const router = useRouter()
     const [calendar, updateCalendar] = calendarState
     const [data, setData] = useState([])
-    const [firstDayOfMonth, setFirstDayOfMonth] = useState(0)
-    const [lastDateOfMonth, setLastDateOfMonth] = useState(0)
     const [showReportViewer, setShowReportViewer] = useState(false)
 
-    const normalize = (UTCDate) => new Date(UTCDate.getUTCFullYear(), UTCDate.getUTCMonth(), UTCDate.getUTCDate())
+    const today = useMemo(() => DateTime.now(), [])
+    const firstOfMonth = useMemo(() => DateTime.local(calendar.year, calendar.month + 1, 1), [calendar])
+    const lastOfMonth = useMemo(() => DateTime.local(calendar.year, calendar.month + 1, 1).endOf('month'), [calendar])
+    const firstDayOfMonth = useMemo(() => firstOfMonth.weekday % 6, [firstOfMonth])
+    const lastOfPreviousMonth = useMemo(() => firstOfMonth.minus({days: 1}), [firstOfMonth])
+    const lastDateOfMonth = useMemo(() => lastOfMonth.day, [lastOfMonth])
+
+    const missingReportDates = useMemo(() => stats.missingReportDates?.map((date) => DateTime.fromISO(date).day).sort((a, b) => a - b), [stats])
+
+    const jobStatusPerDay = useMemo(() => {
+        const statuses = Array(lastDateOfMonth + 1)
+        if (stats.statusHistory) Object.entries(stats.statusHistory).forEach(([key, value]) => {
+            value.forEach((interval) => {
+                let startDate = DateTime.fromISO(interval.startDate)
+                let endDate = DateTime.fromISO(interval.endDate)
+
+                if (startDate < firstOfMonth) startDate = firstOfMonth
+                if (endDate > lastOfMonth)  endDate = lastOfMonth
+                if (startDate > endDate) startDate = endDate
+
+                Range(startDate.day, endDate.day + 1).forEach((day) => statuses[day] = key)
+            })
+        })
+        return statuses
+    }, [stats, firstOfMonth, lastOfMonth, lastDateOfMonth])
+
+    const lastDateOfPreviousMonth = lastOfPreviousMonth.day
+
+    const reportDates = data.map((report) => DateTime.fromISO(report.date).day).sort((a, b) => a - b)
+
+    const missingReportOnClick = (date) => {
+        router.push('/report?' + new URLSearchParams({job: job.id, date: `${calendar.year.toString()}-${(calendar.month + 1).toString().padStart(2, '0')}-${date.toString().padStart(2, '0')}`}))
+    }
+
+    const reportOnClick = (report) => {
+        if (report) setShowReportViewer(report.date)
+    }
+
+    let missingReportCounter = 0
+    let reportCounter = 0
+    let monthDays = Range(1, lastDateOfMonth + 1).map((v) => {
+        const report = reportDates[reportCounter] === v ? data[reportCounter++] : null
+        const reportMissing = missingReportDates ? missingReportDates[missingReportCounter] === v : false
+        if (reportMissing) missingReportCounter++
+        const cursor = reportMissing || !!report ? {cursor: 'pointer'} : {}
+        return {
+            date: v,
+            sx: {
+                background: '#FAF9F9',
+                ...cursor,
+            },
+            dateSX: {
+                color: 'var(--joy-palette-primary-900)'
+            },
+            reportMissing: reportMissing,
+            status: jobStatusPerDay[v],
+            report: report,
+            currentCalendarMonth: true,
+            onClick: reportMissing && !report ? () => missingReportOnClick(v) : () => reportOnClick(report)
+        }
+    })
+
+    const createFillIns = (start, end, update) => {
+        return Range(start, end + 1).map((v) => {
+            return {
+                date: v,
+                sx: {
+                    background: 'var(--joy-palette-neutral-200)',
+                    '&:hover': {
+                        background: 'var(--joy-palette-neutral-500)'
+                    },
+                    cursor: 'pointer'
+                },
+                currentCalendarMonth: false,
+                onClick: () => updateCalendar(update)
+            }
+        });
+    }
+
+    const anteFillIns = firstDayOfMonth > 0 ?
+        createFillIns(lastDateOfPreviousMonth - firstDayOfMonth + 1, lastDateOfPreviousMonth, -1)
+        : createFillIns(0, -1, -1);
+
+    const postFillIns = createFillIns(1, 42 - monthDays.size - anteFillIns.size, 1);
+
+    const preparedData = [...anteFillIns, ...monthDays, ...postFillIns]
+
+    const findDateIndex = (date) => date.month - 1 === calendar.month && date.year === calendar.year ? date.day + firstDayOfMonth - 1 : null
+
+    const todayIndex = findDateIndex(today)
+    const startDateIndex = findDateIndex(DateTime.fromISO(job.startDate))
 
     useEffect(() => {
-        const firstOfMonth = new Date(calendar.year, calendar.month, 1)
+        const formatDate = (date) => date.toFormat('yyyy-MM-dd')
 
-        const lastOfMonth = new Date(calendar.year, calendar.month + 1, 1)
-        lastOfMonth.setDate(0)
-
-        let today = new Date()
-        today = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-
-        const prepareData = (reports) => {
-            const firstDayOfMonth = firstOfMonth.getDay()
-            setFirstDayOfMonth(firstDayOfMonth)
-
-            const lastOfPreviousMonth = new Date(firstOfMonth)
-            lastOfPreviousMonth.setDate(0)
-
-            const lastDateOfPreviousMonth = lastOfPreviousMonth.getDate()
-
-            const lastDateOfMonth = lastOfMonth.getDate()
-            setLastDateOfMonth(lastDateOfMonth)
-
-            const jobStatusPerDay = Array(lastDateOfMonth + 1)
-            if (stats.statusHistory) Object.entries(stats.statusHistory).forEach(([key, value]) => {
-                value.forEach((interval) => {
-                    let startDate = normalize(new Date(interval.startDate))
-                    let endDate = normalize(new Date(interval.endDate))
-
-                    if (startDate < firstOfMonth) startDate = firstOfMonth
-                    if (endDate > lastOfMonth)  endDate = lastOfMonth
-                    if (startDate > endDate) startDate = endDate
-
-                    Range(startDate.getDate(), endDate.getDate() + 1).forEach((day) => jobStatusPerDay[day] = key)
-                })
+        if (job && today && firstOfMonth && lastOfMonth) {
+            postman.get('/reports/summarized?' + new URLSearchParams({
+                job: job.id,
+                startDate: formatDate(firstOfMonth),
+                endDate: formatDate(lastOfMonth > today ? today : lastOfMonth)
+            })).then((response) => {
+                setData(response.data)
+            }).catch((error) => {
+                setSnackbar('error', {text: 'An error occurred while loading job data.'})
             })
-
-            const missingReportDates = stats.missingReportDates?.map((date) => new Date(date).getUTCDate()).sort((a, b) => a - b)
-            const reportDates = reports.map((report) => new Date(report.date).getUTCDate()).sort((a, b) => a - b)
-
-            const missingReportOnClick = (date) => {
-                router.push('/report?' + new URLSearchParams({job: job.id, date: `${calendar.year.toString()}-${(calendar.month + 1).toString().padStart(2, '0')}-${date.toString().padStart(2, '0')}`}))
-            }
-
-            const reportOnClick = (report) => {
-                if (report) setShowReportViewer(report.date)
-            }
-
-            let missingReportCounter = 0
-            let reportCounter = 0
-            let monthDays = Range(1, lastDateOfMonth + 1).map((v) => {
-                const report = reportDates[reportCounter] === v ? reports[reportCounter++] : null
-                const reportMissing = missingReportDates ? missingReportDates[missingReportCounter] === v : false
-                if (reportMissing) missingReportCounter++
-                const cursor = reportMissing || !!report ? {cursor: 'pointer'} : {}
-                return {
-                    date: v,
-                    sx: {
-                        background: '#FAF9F9',
-                        ...cursor,
-                    },
-                    dateSX: {
-                        color: 'var(--joy-palette-primary-900)'
-                    },
-                    reportMissing: reportMissing,
-                    status: jobStatusPerDay[v],
-                    report: report,
-                    currentCalendarMonth: true,
-                    onClick: reportMissing && !report ? () => missingReportOnClick(v) : () => reportOnClick(report)
-                }
-            })
-
-            const createFillIns = (start, end, update) => {
-                return Range(start, end + 1).map((v) => {
-                    return {
-                        date: v,
-                        sx: {
-                            background: 'var(--joy-palette-neutral-200)',
-                            '&:hover': {
-                                background: 'var(--joy-palette-neutral-500)'
-                            },
-                            cursor: 'pointer'
-                        },
-                        currentCalendarMonth: false,
-                        onClick: () => updateCalendar(update)
-                    }
-                });
-            }
-
-            const anteFillIns = firstDayOfMonth > 0 ?
-                createFillIns(lastDateOfPreviousMonth - firstDayOfMonth + 1, lastDateOfPreviousMonth, -1)
-                : createFillIns(0, -1, -1);
-
-            const postFillIns = createFillIns(1, 42 - monthDays.size - anteFillIns.size, 1);
-
-            return [...anteFillIns, ...monthDays, ...postFillIns]
         }
-
-        const formatDate = (date) => {
-            return date.toLocaleString("en-CA", {timeZone: "UTC", month: '2-digit', day: '2-digit', year: 'numeric'})
-        }
-
-        postman.get('/reports/summarized?' + new URLSearchParams({
-            job: job.id,
-            startDate: formatDate(firstOfMonth),
-            endDate: formatDate(lastOfMonth > today ? today : lastOfMonth)
-        })).then((response) => {
-            if (response.status === 200) {
-                setData(prepareData(response.data))
-            } else {
-                console.log(response)
-                // todo implement error handling
-            }
-        }).catch((error) => {
-            console.log(error)
-            // todo implement error handling
-        })
-
-    }, [calendar, updateCalendar, stats, job, router])
-
-    const findDateIndex = (date) => date.getMonth() === calendar.month && date.getFullYear() === calendar.year ? date.getDate() + firstDayOfMonth - 1 : null
-
-    const todayIndex = findDateIndex(new Date())
-    const startDateIndex = findDateIndex(normalize(new Date(job.startDate)))
+    }, [job, today, firstOfMonth, lastOfMonth, setSnackbar])
 
     return <Box sx={{...sx, display: 'flex', flexDirection: 'column', height: '100%', userSelect: 'none', WebkitUserSelect: 'none'}}>
         <CalendarControl sx={{my: 1, flex: '0 1 auto'}} calendarState={calendarState}/>
@@ -163,7 +148,7 @@ export default function Calendar({sx, calendarState, stats}) {
                     }>{day}</Typography>)}
 
             </Box>
-            {Range(0, 6).map((i) => Range(0, 7).map((j) => <CalendarDate key={i*10 + j} sx={{gridRow: i+2, gridColumn: j+1}} data={data[i*7 + j]} metadata={{index: i*7 + j, todayIndex: todayIndex, startDateIndex: startDateIndex}} />))}
+            {Range(0, 6).map((i) => Range(0, 7).map((j) => <CalendarDate key={i*10 + j} sx={{gridRow: i+2, gridColumn: j+1}} data={preparedData[i*7 + j]} metadata={{index: i*7 + j, todayIndex: todayIndex, startDateIndex: startDateIndex}} />))}
         </Box>
         <ReportViewer open={!!showReportViewer} onClose={() => setShowReportViewer(false)} date={showReportViewer} />
     </Box>
