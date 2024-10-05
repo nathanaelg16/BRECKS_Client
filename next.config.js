cea31a0024de6719f48343f55e177a3fcc17df11
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-10-05 17:50:23 -0400

    removing CDN references

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {{images: {remotePatterns: string[]}}} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.brecks.app',
-                pathname: '/logos/**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'brecks.nyc3.cdn.digitaloceanspaces.com',
-                pathname: '/images/**'
-            }
-        ]
-    }
+
 }
 
 module.exports = nextConfig
